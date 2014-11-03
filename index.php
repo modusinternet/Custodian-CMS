f9e8e830ee2cc71ed448b0f704de961a0866811d
Vincent Hallberg <info@modusinternet.com>
2014-11-03 14:14:26 -0800

    Added ETag Support and another GoDaddy fix
    - Added 'FileETag MTime Size' to the .htaccess file to help support
    better caching.
    - Added code to /index.php to help fix a problem with GoDaddy servers
    returning the incorrect DOCUMENT_ROOT for subdomains hosted from within
    the folders of other active sites.

--- index.php
+++ index.php
@@ -147,5 +147,10 @@ if($CFG["DEBUG"] == 1) {
 	echo "</pre>]</span><br />\n";
 }
 
+// Necessary to solve a problem on GoDaddy servers when running sites found in sub folders of existing sites.
+if($_SERVER["REAL_DOCUMENT_ROOT"]) {
+	$_SERVER["DOCUMENT_ROOT"] = $_SERVER["REAL_DOCUMENT_ROOT"];
+}
+
 CCMS_go();
 ?>
