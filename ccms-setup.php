b449d6246e3e69d86de69b05ff0dff625f5568ab
Vincent Hallberg <vince@modusinternet.com>
2018-07-01 14:24:21 -0700

    Copyright update

--- ccms-setup.php
+++ ccms-setup.php
@@ -475,7 +475,7 @@ if($CFG["DBH"]) {
 				</div>
 				<div class="tab-pane fade" id="copyright">
 					<h1>The MIT License (MIT)</h1>
-					<p>Copyright &copy; <?php echo date("Y");?> Vincent Hallberg</p>
+					<p>Copyright &copy; <?php echo date("Y");?> assigned by Vincent Hallberg of <a class='oj' href="https://custodiancms.org" target="_blank">Custodiancms.org</a> and <a class='oj' href="https://modusinternet.com" target="_blank">Modusinternet.com</a></p>
 					<p>Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:</p>
 					<p>The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.</p>
 					<p>THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.</p>
