5853ccbf982bd173ec5b35487b73fc846fc86748
Vincent Hallberg <vince@modusinternet.com>
2018-07-07 13:11:21 -0700

    Release 0.6.4

--- ccmspre/whitelist_user_original.php
+++ ccmspre/whitelist_user_original.php
@@ -18,15 +18,15 @@ A list of predefined PHP constants for use with the filter_var() function can be
 The following is a list of types already pre defined that you can use and the regular expressions they represent.
 These types are found at the top of the /ccmspre/index.php template and should not be altered there.
 
-CRYPT							=>	/^[a-z-_/#=&:\pN\?\.\";\'\`\*\s]*\z/i
-HTTP_ACCEPT_LANGUAGE			=>	/^[a-z0-9-,;=\.]{2,}\z/i
-HTTP_COOKIE						=>	/^[a-z-_=\.\pN]{1,}\z/i
-LNG								=>	/^[a-z]{2}(-[a-z]{2})?\z/i
-PARMS							=>	/^[a-z-_\pN/]+\z/i
-QUERY_STRING					=>	/^[a-z-_=&\.\pN]{1,}\z/i
-SESSION_ID						=>	/^[a-z0-9]{1,}\z/i
-TPL								=>	/^[a-z-\pN\/]{1,}\z/i
-UTF8_STRING_WHITE				=>	/^[\pL\pM*+\s]*\z/u
+CRYPT									=>	/^[a-z-_/#=&:\pN\?\.\";\'\`\*\s]*\z/i
+HTTP_ACCEPT_LANGUAGE				=>	/^[a-z0-9-,;=\.]{2,}\z/i
+HTTP_COOKIE							=>	/^[a-z-_=\.\pN]{1,}\z/i
+LNG									=>	/^[a-z]{2}(-[a-z]{2})?\z/i
+PARMS									=>	/^[a-z-_\pN/]+\z/i
+QUERY_STRING						=>	/^[a-z-_=&\.\pN]{1,}\z/i
+SESSION_ID							=>	/^[a-z0-9]{1,}\z/i
+TPL									=>	/^[a-z-\pN\/]{1,}\z/i
+UTF8_STRING_WHITE					=>	/^[\pL\pM*+\s]*\z/u
 UTF8_STRING_DIGIT_WHITE			=>	/^[\pL\pM*+\pN\s]*\z/u
 UTF8_STRING_DIGIT_PUNC_WHITE	=>	/^[\pL\pM*+\pN\pP\s]*\z/u
 
@@ -92,81 +92,102 @@ define('ANY', '/^(.*)\z/s');
 // /		End of the Pattern.
 // s		Newlines
 
+define('G_RECAPTCHA_RESPONSE', '/^[a-z\pN-_]*\z/i');
+// ^		Start of line
+// [		Start of the character class.
+// a-z		Any kind of letter from any language, upper or lower case.
+// \pN		Any number.
+// -		Hyphans
+// _		Underscore
+// ]		End of the character class.
+// \z		End of subject or newline at end. (Better then $ because $ does not include /n characters at the end of a line.)
+// /i		Case insensitive
+
 $whitelist = array(
-    "example_given_name"    => array("type" => "EXAMPLE_EXPRESSION_1",  "minlength" => 1,   "maxlength" => 15),
-    "example_age"           => array("type" => "EXAMPLE_EXPRESSION_2",  "maxlength" => 3),
+	"example_given_name"					=> array("type" => "EXAMPLE_EXPRESSION_1",	"minlength" => 1,		"maxlength" => 15),
+	"example_age"							=> array("type" => "EXAMPLE_EXPRESSION_2",	"maxlength" => 3),
 
-    "logout"                => array("type" => "WHOLE_NUMBER",  "maxlength" => 1),
-    "login"                     => array("type" => "WHOLE_NUMBER",  "maxlength" => 1),
-    "loginEmail"            => array("type" => "EMAIL",             "maxlength" => 128),
-    "loginPassword"             => array("type" => "PASSWORD",      "minlength" => 8),
+	"logout"									=> array("type" => "WHOLE_NUMBER",				"maxlength" => 1),
+	"login"									=> array("type" => "WHOLE_NUMBER",				"maxlength" => 1),
+	"loginEmail"							=> array("type" => "EMAIL",						"maxlength" => 128),
+	"loginPassword"						=> array("type" => "PASSWORD",					"minlength" => 8),
 
-    "ajax_flag"                 => array("type" => "WHOLE_NUMBER",  "maxlength" => 1),
+	"ajax_flag"								=> array("type" => "WHOLE_NUMBER",				"maxlength" => 1),
 
-    "firstname"                 => array("type" => "NO_BADCHARS",   "maxlength" => 64),
-    "lastname"              => array("type" => "NO_BADCHARS",   "maxlength" => 64),
-    "alias"                     => array("type" => "NO_BADCHARS",   "maxlength" => 32),
-    "position"              => array("type" => "NO_BADCHARS",   "maxlength" => 64),
-    "address1"              => array("type" => "NO_BADCHARS",   "maxlength" => 128),
-    "address2"              => array("type" => "NO_BADCHARS",   "maxlength" => 128),
-    "prov_state"            => array("type" => "NO_BADCHARS",   "maxlength" => 32),
-    "country"               => array("type" => "NO_BADCHARS",   "maxlength" => 64),
-    "post_zip"              => array("type" => "NO_BADCHARS",   "maxlength" => 32),
-    "email"                     => array("type" => "EMAIL",             "maxlength" => 255),
-    "phone1"                => array("type" => "NO_BADCHARS",   "maxlength" => 64),
-    "phone2"                => array("type" => "NO_BADCHARS",   "maxlength" => 64),
-    "skype"                     => array("type" => "NO_BADCHARS",   "maxlength" => 32),
-    "facebook"              => array("type" => "NO_BADCHARS",   "maxlength" => 128),
-    "note"                  => array("type" => "NO_BADCHARS",   "maxlength" => 1024),
+	"firstname"								=> array("type" => "NO_BADCHARS",				"maxlength" => 64),
+	"lastname"								=> array("type" => "NO_BADCHARS",				"maxlength" => 64),
+	"alias"									=> array("type" => "NO_BADCHARS",				"maxlength" => 32),
+	"position"								=> array("type" => "NO_BADCHARS",				"maxlength" => 64),
+	"address1"								=> array("type" => "NO_BADCHARS",				"maxlength" => 128),
+	"address2"								=> array("type" => "NO_BADCHARS",				"maxlength" => 128),
+	"prov_state"							=> array("type" => "NO_BADCHARS",				"maxlength" => 32),
+	"country"								=> array("type" => "NO_BADCHARS",				"maxlength" => 64),
+	"post_zip"								=> array("type" => "NO_BADCHARS",				"maxlength" => 32),
+	"email"									=> array("type" => "EMAIL",						"maxlength" => 255),
+	"phone1"									=> array("type" => "NO_BADCHARS",				"maxlength" => 64),
+	"phone2"									=> array("type" => "NO_BADCHARS",				"maxlength" => 64),
+	"skype"									=> array("type" => "NO_BADCHARS",				"maxlength" => 32),
+	"facebook"								=> array("type" => "NO_BADCHARS",				"maxlength" => 128),
+	"note"									=> array("type" => "NO_BADCHARS",				"maxlength" => 1024),
 
-    "password"              => array("type" => "PASSWORD",      "minlength" => 8),
-    "password1"                 => array("type" => "PASSWORD",      "minlength" => 8),
-    "password2"                 => array("type" => "PASSWORD",      "minlength" => 8),
+	"password"								=> array("type" => "PASSWORD",					"minlength" => 8),
+	"password1"								=> array("type" => "PASSWORD",					"minlength" => 8),
+	"password2"								=> array("type" => "PASSWORD",					"minlength" => 8),
 
-    "ccms_ins_db_id"        => array("type" => "WHOLE_NUMBER",  "minlength" => 1,   "maxlength" => 11),
-    "ccms_ins_db_text"      => array("type" => "ANY",           "maxlength" => 16000),
+	"ccms_ins_db_id"						=> array("type" => "WHOLE_NUMBER",				"minlength" => 1,		"maxlength" => 11),
+	"ccms_ins_db_text"					=> array("type" => "ANY",							"maxlength" => 16000),
+
+	"ccms_pass_reset_form"				=> array("type" => "WHOLE_NUMBER",				"maxlength" => 1),
+	"ccms_pass_reset_form_email"		=> array("type" => "EMAIL",						"maxlength" => 255),
+	"g-recaptcha-response"				=> array("type" => "G_RECAPTCHA_RESPONSE",	"maxlength" => 2048),
+	"ccms_pass_reset_form_prf"			=> array("type" => "WHOLE_NUMBER",				"maxlength" => 1),
+	"ccms_pass_reset_form_code"		=> array("type" => "SESSION_ID",					"maxlength"	=> 64)
 );
 
 
-function CCMS_User_Filter($input, $whitelist)
-{
-    global $CLEAN;
-    foreach ($input as $key => $value) {
-        if (array_key_exists($key, $whitelist)) {
-            $buf = null;
-            $value = @trim($value);
-            // utf8_decode() converts unknown ISO-8859-1 chars to '?' for the purpose of counting.
-            $length = strlen(utf8_decode($value));
-            if (isset($whitelist[$key]['minlength']) && ($length < $whitelist[$key]['minlength'])) {
-                $buf = "MINLEN";
-            }
-            if (isset($whitelist[$key]['maxlength']) && ($length > $whitelist[$key]['maxlength'])) {
-                $buf = "MAXLEN";
-            }
-            if ($buf != "MINLEN" && $buf != "MAXLEN") {
-                switch ($whitelist[$key]['type']) {
-
-                    case "EXAMPLE_EXPRESSION_1":
-                        $buf = (preg_match(EXAMPLE_EXPRESSION_1, $value)) ? $value : "INVAL";
-                        break;
-                    case "EXAMPLE_EXPRESSION_2":
-                        $buf = (preg_match(EXAMPLE_EXPRESSION_2, $value)) ? $value : "INVAL";
-                        break;
-                    case "WHOLE_NUMBER":
-                        $buf = (preg_match(WHOLE_NUMBER, $value)) ? $value : "INVAL";
-                        break;
-                    case "EMAIL":
-                        $buf = (preg_match(EMAIL, $value)) ? $value : "INVAL";
-                        break;
-                    case "PASSWORD":
-                        $buf = (preg_match(PASSWORD, $value)) ? $value : "INVAL";
-                        break;
-                    case "NO_BADCHARS":
-                        $buf = (preg_match(NO_BADCHARS, $value)) ? $value : "INVAL";
-                        break;
-                    case "ANY":
-                        $buf = (preg_match(ANY, $value)) ? $value : "INVAL";
-                        break;
+function CCMS_User_Filter($input, $whitelist) {
+	global $CLEAN;
+	foreach ($input as $key => $value) {
+	  if (array_key_exists($key, $whitelist)) {
+			$buf = null;
+			$value = @trim($value);
+			// utf8_decode() converts unknown ISO-8859-1 chars to '?' for the purpose of counting.
+			$length = strlen(utf8_decode($value));
+			if(isset($whitelist[$key]['minlength']) && ($length < $whitelist[$key]['minlength'])) {
+				$buf = "MINLEN";
+			}
+			if(isset($whitelist[$key]['maxlength']) && ($length > $whitelist[$key]['maxlength'])) {
+				$buf = "MAXLEN";
+			}
+			if($buf != "MINLEN" && $buf != "MAXLEN") {
+				switch ($whitelist[$key]['type']) {
+					case "EXAMPLE_EXPRESSION_1":
+						$buf = (preg_match(EXAMPLE_EXPRESSION_1, $value)) ? $value : "INVAL";
+						break;
+					case "EXAMPLE_EXPRESSION_2":
+						$buf = (preg_match(EXAMPLE_EXPRESSION_2, $value)) ? $value : "INVAL";
+						break;
+					case "WHOLE_NUMBER":
+						$buf = (preg_match(WHOLE_NUMBER, $value)) ? $value : "INVAL";
+						break;
+					case "EMAIL":
+						$buf = (preg_match(EMAIL, $value)) ? $value : "INVAL";
+						break;
+					case "PASSWORD":
+						$buf = (preg_match(PASSWORD, $value)) ? $value : "INVAL";
+						break;
+					case "NO_BADCHARS":
+						$buf = (preg_match(NO_BADCHARS, $value)) ? $value : "INVAL";
+						break;
+					case "ANY":
+						$buf = (preg_match(ANY, $value)) ? $value : "INVAL";
+						break;
+					case "G_RECAPTCHA_RESPONSE":
+						$buf = (preg_match(G_RECAPTCHA_RESPONSE, $value)) ? $value : "INVAL";
+						break;
+					case "SESSION_ID":
+						$buf = (preg_match(SESSION_ID, $value)) ? $value : "INVAL";
+						break;
 
 // Add your own case statements here, just copy the patter above, make the neccessary changes, save and upload.
 
