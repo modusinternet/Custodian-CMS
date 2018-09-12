c5fa4974e1d5f791225ad8b8fee0b87ef21f759d
Vincent Hallberg <vince@modusinternet.com>
2018-09-12 12:26:25 -0700

    New feature to automatically pull updates for your website from a GitHub repo using a webhook.

---
+++ ccmsusr/github/auto-pull-from-github.php
@@ -0,0 +1,9 @@
+<?php
+header("Content-Type: text/html; charset=UTF-8");
+header("Expires: on, 01 Jan 1970 00:00:00 GMT");
+header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+header("Cache-Control: no-store, no-cache, must-revalidate");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+
+echo "<pre>`git pull`</pre>"
