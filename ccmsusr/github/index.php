b58b94a1477309465b7b04df0547244f1bdef1c8
Vincent Hallberg <vince@modusinternet.com>
2018-09-14 15:16:17 -0700

    instruction update

--- ccmsusr/github/index.php
+++ ccmsusr/github/index.php
@@ -36,8 +36,8 @@ if($_SERVER["SCRIPT_NAME"] != "/ccmsusr/index.php") {
 							<div class="panel-footer">
 								<pre style="padding: unset; margin: unset; border: unset;">
 - create a new repository at GitHub
-- add your web servers public ssh-key to your new repo on GitHub under 'Settings/Deploy keys' (follow instructions here to generate a new ssh-key if needed: https://help.github.com/articles/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent/)
-- add a webhook on GitHub under 'Settings/Webhooks' to 'https://YOUR_DOMAIN/ccmsusr/github/webhook.php'
+- add your web servers public ssh-key to your new repo on GitHub under 'Settings/Deploy keys', with 'Allow write access' checked (follow instructions here to generate a new ssh-key if needed: https://help.github.com/articles/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent/)
+- add a webhook on GitHub under 'Settings/Webhooks' to 'https://YOUR_DOMAIN/ccmsusr/github/webhook.php' (ignor 404 error on first attempt to connect)
 - create a new website folder on your server (must have access to ssh and git services)
 - ssh into it and type 'git clone git@github.com:modusinternet/Custodian-CMS.git' to pull down the repository
 - from the command line on your server type the following
