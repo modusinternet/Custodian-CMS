a896df51d71cb69182dc3d93948018a4323f8c26
modusinternet <vince@modusinternet.com>
2021-10-22 04:33:23 -0700

    increased the use of the isset() function

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ $CFG["VERSION"] = "0.7.2";
 $CFG["RELEASE_DATE"] = "Oct 18, 2021";
 
 // Necessary to solve a problem on GoDaddy servers when running sites found in sub folders of existing sites.
-if($_SERVER["REAL_DOCUMENT_ROOT"]) {
+if(isset($_SERVER["REAL_DOCUMENT_ROOT"])) {
 	$_SERVER["DOCUMENT_ROOT"] = $_SERVER["REAL_DOCUMENT_ROOT"];
 }
 
